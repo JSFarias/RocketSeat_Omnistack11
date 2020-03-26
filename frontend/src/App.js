3cc314601f7a68c1dee89038f45bc58332ea0c79
JSFarias <jhonatas.s.farias@gmail.com>
2020-03-26 17:57:47 -0300

    logon & register pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-import Header from './Header'
+import './global.css'
+
+import Routes from './routes'
 
 function App() {
   return (
-   <Header>Jhonatas Farias</Header>
+    <>
+      <Routes />
+    </>
   );
 }
 
